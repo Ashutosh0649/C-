64f28a35a45359641e1471f07750a273002241a3
Ashutosh0649 <74145687+Ashutosh0649@users.noreply.github.com>
2020-12-16 10:57:33 +0530

    Cube of number

---
+++ cube_of_number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+int main()
+{
+    long int val , cub;
+  cout<<"Enter The number whose cudeto be find"<<endl;
+  cin>>val;
+
+  cub=pow(val,3);
+ 
+  cout<<cub;
+
+
+}
