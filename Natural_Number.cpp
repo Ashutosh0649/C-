e31468ef5f590d6f11188cd52ef19cff88b3ea23
Ashutosh0649 <74145687+Ashutosh0649@users.noreply.github.com>
2020-12-16 10:52:17 +0530

    Natural Number
    It is a code finding the Natural Number.

---
+++ Natural_Number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout <<"First 10 Natural Number are  "<< endl;
+ 
+    for(int i=1; i<=10; i++)
+    {
+        cout<< i<< " ";
+    }
+
+}
